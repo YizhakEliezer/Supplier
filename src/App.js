e8d43452c673bd5211d87a721f374b2c415432dc
Yizhak Eliezer <yizhake@storenext.co.il>
2024-04-18 08:50:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import MainComponent from './comp/main';
+
+function App() {
+    return (
+      
+        <div>
+
+            <MainComponent />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/comp/main.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import './main.css';
+import logo from './Logo.png';
+
+function MainComponent() {
+    const [organizations, setOrganizations] = useState([]);
+    const [selectedOrganization, setSelectedOrganization] = useState('');
+    const [selectedSupplier, setSelectedSupplier] = useState({ key: '', value: '' });
+    const [selectedSupplierKey, setSelectedSupplierKey] = useState('');
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('/refael.json');
+                const data = await response.json();
+                setOrganizations(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const handleOrganizationSelect = (value) => {
+        const org = organizations.find(org => Object.keys(org)[0] === value);
+        if (org && Object.keys(org).length > 0) {
+            setSelectedOrganization(value);
+            if (!selectedSupplier.value) {
+                setSelectedSupplierKey(''); // אם המשתמש עדיין לא בחר ספק, נקבע את מפתח הספק לריק
+            } else {
+                setSelectedSupplierKey(org[Object.keys(org)[0]][0].key);
+            }
+        } else {
+            setSelectedOrganization('');
+            setSelectedSupplier({ key: '', value: '' });
+            setSelectedSupplierKey('');
+        }
+    };
+    
+    
+    
+    const handleSupplierChange = (e) => {
+        const { value } = e.target;
+        const selectedOrg = organizations.find(org => Object.keys(org)[0] === selectedOrganization);
+        const selectedSupplier = selectedOrg[selectedOrganization].find(supplier => supplier.value === value);
+        if (selectedSupplier) {
+            setSelectedSupplier(selectedSupplier);
+            setSelectedSupplierKey(selectedSupplier.key);
+        } else {
+            setSelectedSupplier({ key: '', value: '' }); // למקרה שהספק לא נמצא
+            setSelectedSupplierKey(''); // גם מוחקים את הערך הקודם במקרה שהספק לא נמצא
+        }
+    };
+    
+    
+
+    return (
+        <div className="container">
+            <div className="titel-container"></div>
+            <img src={logo} alt="logo"></img>
+            <div className="main-container">
+                <input
+                    type="text"
+                    placeholder="ארגון"
+                    className="retailer"
+                    list="organizationsList"
+                    onChange={(e) => handleOrganizationSelect(e.target.value)}
+                />
+                <datalist id="organizationsList">
+                    {organizations.map((org, index) => (
+                        <option key={index} value={Object.keys(org)[0]} />
+                    ))}
+                </datalist>
+                <input
+                    type="text"
+                    placeholder="ספק"
+                    className="supplier"
+                    list="suppliersList"
+                    onChange={handleSupplierChange}
+                />
+                 
+                 <input
+                    type="text"
+                    placeholder="מספר ספק"
+                    className="numSupplier"
+                    value={selectedSupplierKey}
+                    readOnly
+                />
+           
+                <datalist id="suppliersList">
+                    {selectedOrganization &&
+                        organizations
+                            .find(org => Object.keys(org)[0] === selectedOrganization)
+                            [selectedOrganization].map((supplier, index) => (
+                                <option key={index} value={supplier.value} />
+                            ))}
+                </datalist>
+            </div>
+        </div>
+    );
+}
+
+export default MainComponent;
